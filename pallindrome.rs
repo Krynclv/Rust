cd0d6412369737bae9fb5df236a714fe012fba1a
Keryan <99647411+Krynclv@users.noreply.github.com>
2022-03-07 11:50:50 +0100

    Add files via upload

---
+++ pallindrome.rs
@@ -0,0 +1,9 @@
+fn main() {}
+fn is_pallindrome(mot:&str) -> bool {
+    let inverse = mot.chars().rev().collect::<String>();
+    if mot == inverse {
+        true
+    } else {
+        false
+    }
+}
